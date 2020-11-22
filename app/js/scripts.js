a790300eabf7693cdcb12eeb29f8cfc746772277
Evgeniy Sannikov <sann-zhenya@yandex.ru>
2020-11-22 23:42:06 +0300

    sort scripts

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,12 +1,33 @@
 document.querySelector(".block_2_form_email").addEventListener("click", ()=>{
     document.querySelector(".block_2_form_email").value ="";
 });
+let cards = document.querySelector(".cards");
 
+let card = document.querySelectorAll(".card");
 
- 
+let button1 = document.querySelector("#price");
 
+let button2 = document.querySelector("#age");
 
+let arr = [...card];
 
+    button1.addEventListener("click", ()=> {
+        arr.sort(function(a,b) {
+            return parseInt(a.children[3].innerHTML) - parseInt(b.children[3].innerHTML);
+        });
+        cards.innerHTML = "";
 
+        for(let item of arr) {
+            cards.appendChild(item);
+        }});
 
+    button2.addEventListener("click", ()=>{
+        arr.sort(function(a,b){
+            return parseInt(a.children[2].children[1].children[0].innerHTML) - parseInt(b.children[2].children[1].children[0].innerHTML);
+        })
+        cards.innerHTML = "";
 
+        for(let items of arr) {
+            cards.appendChild(items);
+        }
+    });
